68378f86de1e0d17fa1fe6011552d27ba15b3301
Markus Braun <Markus.Braun@kardex.com>
2013-08-01 14:43:56 +0200

    Added typ "float" in SqlServerDialect

--- src/Migrator.Providers/Impl/SqlServer/SqlServerDialect.cs
+++ src/Migrator.Providers/Impl/SqlServer/SqlServerDialect.cs
@@ -24,10 +24,12 @@ namespace Migrator.Providers.SqlServer
 			RegisterColumnType(DbType.Currency, "MONEY");
 			RegisterColumnType(DbType.Date, "DATETIME");
 			RegisterColumnType(DbType.DateTime, "DATETIME");
-			RegisterColumnType(DbType.Decimal, "DECIMAL(19,5)");
-			RegisterColumnType(DbType.Decimal, 19, "DECIMAL(19, $l)");
-			RegisterColumnType(DbType.Double, "DOUBLE PRECISION"); //synonym for FLOAT(53)
-			RegisterColumnType(DbType.Guid, "UNIQUEIDENTIFIER");
+            RegisterColumnType(DbType.Decimal, "DECIMAL(19,5)");
+            RegisterColumnType(DbType.Decimal, 19, "DECIMAL(19, $l)");
+            RegisterColumnType(DbType.Double, "DOUBLE PRECISION"); //synonym for FLOAT(53)
+            RegisterColumnType(DbType.Double, 24, "FLOAT(24)");
+            RegisterColumnType(DbType.Double, 53, "FLOAT(53)");
+            RegisterColumnType(DbType.Guid, "UNIQUEIDENTIFIER");
 			RegisterColumnType(DbType.Int16, "SMALLINT");
 			RegisterColumnType(DbType.Int32, "INT");
 			RegisterColumnType(DbType.Int64, "BIGINT");
@@ -40,6 +42,8 @@ namespace Migrator.Providers.SqlServer
             RegisterColumnType(DbType.String, int.MaxValue, "NVARCHAR(max)");
 			//RegisterColumnType(DbType.String, 1073741823, "NTEXT");
 			RegisterColumnType(DbType.Time, "DATETIME");
+            RegisterColumnType(DbType.VarNumeric, "NUMERIC(18,0)");
+            RegisterColumnType(DbType.VarNumeric, 38, "NUMERIC($l,0)");
 
 			RegisterProperty(ColumnProperty.Identity, "IDENTITY");
 
