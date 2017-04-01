c36ce11154463d89931e735229cd951387738735
jogibear9988 <jochen.kuehner@gmx.de>
2017-04-01 11:59:29 +0200

    Add DBProviderFactories for NetStandart

--- src/Migrator.Providers/DbProviderFactoriesHelper.cs
+++ src/Migrator.Providers/DbProviderFactoriesHelper.cs
@@ -10,9 +10,6 @@ namespace Migrator.Providers
     {
         public static DbProviderFactory GetFactory(string providerName, string assemblyName, string factoryProviderType)
         {
-#if NETSTANDARD
-            return null;
-#else
             try
             {
                 return DbProviderFactories.GetFactory(providerName);
@@ -20,8 +17,39 @@ namespace Migrator.Providers
             catch(Exception)
             { }
 
+#if NETSTANDARD
+            return null;
+#else
             return (DbProviderFactory)AppDomain.CurrentDomain.CreateInstanceAndUnwrap(assemblyName, factoryProviderType);
 #endif
         }
     }
+
+#if NETSTANDARD
+    public abstract class DbProviderFactories
+    {
+
+        internal static readonly Dictionary<string, Func<DbProviderFactory>> _configs = new Dictionary<string, Func<DbProviderFactory>>();
+
+        public static DbProviderFactory GetFactory(string providerInvariantName)
+        {
+            if (_configs.ContainsKey(providerInvariantName))
+            {
+                return _configs[providerInvariantName]();
+            }
+
+            throw new Exception("ConfigProviderNotFound");
+        }
+
+        public static void RegisterFactory(string providerInvariantName, Func<DbProviderFactory> factory)
+        {
+            _configs[providerInvariantName] = factory;
+        }
+
+        public static IEnumerable<string> GetFactoryProviderNames()
+        {
+            return _configs.Keys.ToArray();
+        }
+    }
+#endif
 }
