4aa437a20e6205d0753e7d4a9b1587473c79b592
jkuehner <jochen.kuehner@kardex.com>
2020-11-17 10:40:03 +0100

    bugfix postgres pkey

--- src/Migrator.Providers/Impl/PostgreSQL/PostgreSQLTransformationProvider.cs
+++ src/Migrator.Providers/Impl/PostgreSQL/PostgreSQLTransformationProvider.cs
@@ -44,7 +44,7 @@ namespace Migrator.Providers.PostgreSQL
 		{
 			using (var cmd = CreateCommand())
 			using (IDataReader reader =
-				ExecuteQuery(cmd, string.Format("SELECT conname FROM pg_constraint WHERE contype = 'p' AND conrelid = (SELECT oid FROM pg_class WHERE relname LIKE '{0}');", table)))
+				ExecuteQuery(cmd, string.Format("SELECT conname FROM pg_constraint WHERE contype = 'p' AND conrelid = (SELECT oid FROM pg_class WHERE relname = lower('{0}'));", table)))
 			{
 				return reader.Read() ? reader.GetString(0) : null;
 			}
